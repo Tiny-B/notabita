156f05b115e645f901f4b236596e799a9d340037
Tiny-b <kiefkostello@gmail.com>
2025-10-27 11:04:26 +0000

    Add carousel banner

--- resources/script/index.js
+++ resources/script/index.js
@@ -4,9 +4,14 @@
 // formatting, add header 1,2,3,3, bullet lists
 // add horizontal line
 // insert links
-// font sizing
-// ai assistant
+// font sizing 1x 2x 3x ect
+// ai assistant helps with current note and can answer questions or give info you need
 // font style and alignment
 // save, edit and delete
 // download note
 // spell checker
+// text highlighting
+// font color
+// theme selector
+// header and footer
+// stats display, word count etc
