ea5865366e371ff74b0edc4e1514e5a4c2d18bdf
Tiny-b <kiefkostello@gmail.com>
2025-10-27 08:07:09 +0000

    Add navbar

---
+++ resources/script/index.js
@@ -0,0 +1,12 @@
+// font selection
+// add images
+// add tables
+// formatting, add header 1,2,3,3, bullet lists
+// add horizontal line
+// insert links
+// font sizing
+// ai assistant
+// font style and alignment
+// save, edit and delete
+// download note
+// spell checker
