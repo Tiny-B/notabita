363776d38307c1d51b0d795697a2f80ee8161b8f
Tiny-b <kiefkostello@gmail.com>
2025-10-29 14:26:19 +0000

    Add editor page layout

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,4 +1,4 @@
-// font selection
+/* font selection
 // add images
 // add tables
 // formatting, add header 1,2,3,3, bullet lists
@@ -14,6 +14,23 @@
 // font color
 // theme selector
 // header and footer
-// stats display, word count etc
+  stats display, word count etc */
+
+const { error } = require('console');
 
 console.info('Hi there');
+
+function reverseString(str) {
+	if (str === '') throw new Error('Cannot be empty');
+	if (typeof str !== 'string') throw new TypeError('Not a string');
+	let reverse = str.split('').reverse().join('');
+	return reverse;
+}
+
+function is_palindrome(str) {
+	if (str === '') throw new Error('Cannot be empty');
+	if (typeof str !== 'string') throw new TypeError('Not a string');
+	return reverseString(str) === str;
+}
+
+module.exports = { reverseString, is_palindrome };
