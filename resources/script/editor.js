a1c652bf9de18a133c3f489a01a80295201dd938
Tiny-b <kiefkostello@gmail.com>
2025-10-30 09:15:12 +0000

    Change align funtion to a format function for reusability

--- resources/script/editor.js
+++ resources/script/editor.js
@@ -6,13 +6,25 @@ const rightAlignBtn = textAlignBtns[2];
 
 const content = document.getElementById('content');
 
-const alignText = tag => {
+const formatText = tag => {
 	const selectedText = window.getSelection();
 	if (selectedText.isCollapsed) return; // nothing being selected
 
 	// Create a container that will hold the selected text
 	const container = document.createElement('div');
-	container.style.textAlign = tag;
+
+	// Determine what kind of formatting we need
+	switch (tag) {
+		case 'left':
+			container.style.textAlign = tag;
+			break;
+		case 'center':
+			container.style.textAlign = tag;
+			break;
+		case 'right':
+			container.style.textAlign = tag;
+			break;
+	}
 
 	// Pull the selected contents out of the DOM
 	const range = selectedText.getRangeAt(0);
@@ -20,7 +32,7 @@ const alignText = tag => {
 	// Check if text selected is text we allow to be edited
 	const is_editable_text = content.contains(range.commonAncestorContainer);
 	if (!is_editable_text) {
-		console.error('Selection is not within the content window');
+		//console.error('Selection is not within the content window');
 		return;
 	}
 
@@ -37,11 +49,11 @@ const alignText = tag => {
 };
 
 leftAlignBtn.onclick = function () {
-	alignText('left');
+	formatText('left');
 };
 centerAlignBtn.onclick = function () {
-	alignText('center');
+	formatText('center');
 };
 rightAlignBtn.onclick = function () {
-	alignText('right');
+	formatText('right');
 };
